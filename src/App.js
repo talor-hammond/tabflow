a36df8bab5073925147bd9bd853778d24ff238fb
Talor H <talorjhammond@gmail.com>
2019-01-13 00:27:20 +1300

    standard --fix, linting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div>
         <h1>Hello world</h1>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
