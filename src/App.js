711b8fcf827b6c96b89aba95a9ed0e541c0c3586
Talor H <talorjhammond@gmail.com>
2019-01-14 02:12:11 +1300

    init PopOut component

--- src/App.js
+++ src/App.js
@@ -10,36 +10,13 @@
 import React, { Component } from 'react'
 import './index.css'
 
+// Components:
+import PopOut from './components/PopOut'
+
 class App extends Component {
-  constructor () {
-    super()
-
-    this.state = {
-      // An array of workflows (this might be better suited as an object), users can add
-      // an array of tab objects (with 'pinned', 'urls' set)
-      workflows: []
-    }
-  }
-
-  componentDidMount () {
-    // const tab = {
-    //   // windowId: The window in which to create the new tab. Defaults to the current window.
-    //   // index: The position the tab should take in the window. The provided value is clamped to between zero and the number of tabs in the window.,
-    //   url: 'https://developer.chrome.com/extensions/tabs#method-create',
-    //   active: true,
-    //   pinned: false
-    // }
-
-    // window.chrome.tabs.create(tab)
-  }
-
   render () {
     return (
-      <div style={{ width: 500, height: 800 }}>
-        <h1>tabflow</h1>
-        <hr />
-        <h2>Pre-define the tabs you want for different work-flows.</h2>
-      </div>
+      <PopOut />
     )
   }
 }
---
+++ src/components/PopOut.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class PopOut extends Component {
+  componentDidMount () {
+    // const tab = {
+    //   // windowId: The window in which to create the new tab. Defaults to the current window.
+    //   // index: The position the tab should take in the window. The provided value is clamped to between zero and the number of tabs in the window.,
+    //   url: 'https://developer.chrome.com/extensions/tabs#method-create',
+    //   active: true,
+    //   pinned: false
+    // }
+
+    // window.chrome.tabs.create(tab)
+  }
+
+  render () {
+    return (
+      <div style={{ width: 500, height: 800 }}>
+        <h1>tabflow</h1>
+        <hr />
+        <h2>Pre-define the tabs you want for different work-flows.</h2>
+      </div>
+    )
+  }
+}
+
+export default PopOut
